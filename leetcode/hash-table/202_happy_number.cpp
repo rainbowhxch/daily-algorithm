731d0635267d4c3dc5e1f22ade84dd66b6515ae0
rainbowhxch <rainbowhxch@gmail.com>
2020-09-08 11:22:59 +0800

    leetcode: fixed hash-table:happyNumber

--- leetcode/hash-table/202_happy_number.cpp
+++ leetcode/hash-table/202_happy_number.cpp
@@ -6,8 +6,9 @@ using std::unordered_set;
 bool is_happy(int n) {
 	unordered_set<int> s;
 
-	while (!(s.find(n) == s.end())) {
+	while (s.find(n) == s.end()) {
 		s.insert(n);
+
 		int tmp_sum = 0;
 		while (n > 0) {
 			tmp_sum += std::pow(n%10, 2);
