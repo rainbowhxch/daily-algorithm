695b0b93d3d37dad29e585b72d183600c91caafb
rainbowhxch <rainbowhxch@gmail.com>
2020-10-14 10:07:08 +0800

    array: 1 problems fixed

--- leetcode/array/1304_find_n_unique_integers_sum_up_to_zero.cpp
+++ leetcode/array/1304_find_n_unique_integers_sum_up_to_zero.cpp
@@ -2,16 +2,17 @@
 
 using std::vector;
 
+
 class Solution {
 public:
     vector<int> sumZero(int n) {
         vector<int> ans;
 
         if (n & 1)
-            ans.emplace_back(0);
+            ans.push_back(0);
         for (int i = 1;i <= n/2;++i) {
-            ans.emplace_back(i);
-            ans.emplace_back(-i);
+            ans.push_back(i);
+            ans.push_back(-i);
         }
 
         return ans;
